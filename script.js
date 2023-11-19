5ad6f7a7b6d8a0b7b7c153113c85876f114cf591
abd074 <badawyabdulrahman099@gmail.com>
2023-11-19 17:54:41 +0200

    make it more enhanced with js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let slideImages = [
+let img = document.getElementsByTagName("img")[0];
+
+img.style.cssText =
+  "  display: block;width: 800px;height: 400px;margin: 50px auto;padding: 5px;border: 1px solid #f50;";
+
+// array of src
+let p = document.getElementsByTagName("p")[0];
+
+p.style.cssText =
+  "  width: 60%;margin: auto;background-color: #000;color: #fff;padding: 10px;text-align: center;font-size: 30px;font-weight: bold;";
+
+  let slideImages = [
   "./imgs/img1.jpg",
   "./imgs/img2.jpg",
   "./imgs/img3.webp",
   "./imgs/img4.webp",
   "./imgs/img5.jpg",
 ];
-
-let img = document.getElementsByTagName("img")[0];
 
 let i = 0;
 
